1d3f5e78a8a4ebf01b1356fb4c9c0f3e8078b616
Hugo Quispe Pacaje <hugoquispepacaje@Hugos-MacBook-Pro.local>
2024-07-16 20:38:39 -0400

    fix: migracion de proyecto a react native cli

--- App.tsx
+++ App.tsx
@@ -1,118 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useEffect } from 'react';
+import { BackHandler } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { default as theme } from './theme.json'; 
+import 'react-native-gesture-handler';
+import Navigation from './src/navigation/Navigation';
+import LanguageProvider from './src/context/language/LanguageProvider';
+import GlobalProvider from './src/context/global/GlobalProvider';
+import { MaterialIconsPack } from './src/style/material-icons';
+import { getCurrentScreenName } from './src/navigation/navigationService';
+import ON_BOARDING_ROUTES from './src/feature/onBoarding/navigation/routes';
+import HOME_ROUTES from './src/feature/home/navigation/routes'
+import ROUTES from './src/navigation/routes';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+    useEffect(() => {
+        const backAction = () => {
+            const currentRouteName = getCurrentScreenName();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+            const isBlockBack = [
+                ON_BOARDING_ROUTES.stepOne, 
+                ON_BOARDING_ROUTES.stepTwo,
+                ON_BOARDING_ROUTES.stepThree,
+                ROUTES.initNavigation,
+            ].includes(currentRouteName);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+            if (isBlockBack) return true;
+
+            const isExitApp = currentRouteName === HOME_ROUTES.home;
+            if (isExitApp) {
+                BackHandler.exitApp()
+                return true;
+            }
+
+            return false;
+        };
+
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+        return () => backHandler.remove();
+    }, []);
+
+    return (
+        <>
+            <IconRegistry icons={MaterialIconsPack}/>
+            <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+                <GlobalProvider>
+                    <LanguageProvider>
+                        <Navigation />
+                    </LanguageProvider>
+                </GlobalProvider>
+            </ApplicationProvider>
+        </>
+    );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
