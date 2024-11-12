ca86f25a67922be00c379b8a437309c01e96ba00
Hugo Quispe <hugoquispepacaje@Hugos-MacBook-Pro.local>
2024-11-12 01:46:56 -0300

    feat: multi object ready

--- src/feature/experience/components/ViroContent.js
+++ src/feature/experience/components/ViroContent.js
@@ -1,30 +1,103 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Viro3DObject, ViroARImageMarker, ViroARScene, ViroARSceneNavigator, ViroARTrackingTargets, ViroAmbientLight } from '@reactvision/react-viro';
+import React, { useContext, useEffect, useReducer } from 'react';
+import {
+    Viro3DObject,
+    ViroARImageMarker,
+    ViroARScene,
+    ViroARSceneNavigator,
+    ViroARTrackingTargets,
+    ViroAmbientLight,
+} from '@reactvision/react-viro';
 import ExperienceContext from '../../../context/experience/ExperienceContext';
 
+const initialState = experiences => {
+    const state = {};
+    experiences.forEach(experience => {
+        state[experience.id] = {
+            rotation: [0, 0, 0],
+            scale: [0.5, 0.5, 0.5],
+            position: [0, -16, -1],
+        };
+    });
+    return state;
+};
+
+const reducer = (state, action) => {
+    const { id, type, value } = action;
+    switch (type) {
+        case 'ROTATE':
+            return {
+                ...state,
+                [id]: {
+                    ...state[id],
+                    rotation: value,
+                },
+            };
+        case 'SCALE':
+            return {
+                ...state,
+                [id]: {
+                    ...state[id],
+                    scale: value,
+                },
+            };
+        case 'POSITION':
+            return {
+                ...state,
+                [id]: {
+                    ...state[id],
+                    position: value,
+                },
+            };
+        default:
+            return state;
+    }
+};
 
 const InitialScene = () => {
     const { experiences, targets } = useContext(ExperienceContext);
-    const [rotation, setRotation] = useState([0, 0, 0]);
-    const [scale, setScale] = useState([0.5, 0.5, 0.5]);
-    const position = [0, -16, -1];
+    const [state, dispatch] = useReducer(reducer, experiences, initialState);
 
-    const onRotateObject = (rotateState, rotationFactor) => {
-        if (rotateState === 3) {
-            const currentRotation = [rotation[0] - rotationFactor, rotation[1] - rotationFactor, rotation[2] - rotationFactor];
-            setRotation(currentRotation);
-        }
+    const onMove = (id, { location }) => {
+        dispatch({ id, type: 'POSITION', value: location });
     };
-    
-    const onScaleObject = (pinchState, scaleFactor) => {
+
+    const onScale = (id, pinchState, scaleFactor) => {
         if (pinchState === 3) {
-          let currentScale = scale[0];
-          let newScale = currentScale * scaleFactor;
-          let newScaleArray = [newScale, newScale, newScale];
-          setScale(newScaleArray);
+            const currentScale = state[id].scale[0];
+            const newScaleFactor = scaleFactor > 1 ? 0.1 : -0.1;
+            const newScale = currentScale + newScaleFactor;
+            const newScaleArray = [newScale, newScale, newScale];
+            dispatch({ id, type: 'SCALE', value: newScaleArray });
         }
     };
-    
+
+    const onRotate = (id, rotateState, rotationFactor) => {
+        if (rotateState > 1) {
+            const currentRotation = state[id].rotation;
+            if (rotationFactor > 0) {
+                dispatch({
+                    id,
+                    type: 'ROTATE',
+                    value: [
+                        currentRotation[0] + 10,
+                        currentRotation[1],
+                        currentRotation[2],
+                    ],
+                });
+            } else {
+                dispatch({
+                    id,
+                    type: 'ROTATE',
+                    value: [
+                        currentRotation[0],
+                        currentRotation[1],
+                        currentRotation[2] + 10,
+                    ],
+                });
+            }
+        }
+    };
+
     useEffect(() => {
         ViroARTrackingTargets.createTargets(targets);
     }, []);
@@ -36,15 +109,17 @@ const InitialScene = () => {
                     target={experience.id}
                     key={experience.id}
                     onAnchorFound={() => console.log('se encontro' + experience.text.ES.name)}
-                    onAnchorRemoved={() => console.log('se removio' + experience.text.ES.name)}
                 >
                     <ViroAmbientLight color="#ffffff" />
                     <Viro3DObject
                         source={{ uri : experience.model.url }}
-                        position={[0, -16, -1]}
-                        scale={[0.5, 0.5, 0.5]}
-                        rotation={[-90, 0, 0]}
-                        type="GLB" 
+                        position={state[experience.id].position}
+                        scale={state[experience.id].scale}
+                        rotation={state[experience.id].rotation}
+                        type="GLB"
+                        onDrag={(position) => onMove(experience.id, position)}
+                        onPinch={(pinchState, scaleFactor) => onScale(experience.id, pinchState, scaleFactor)}
+                        onRotate={(rotateState, rotationFactor) => onRotate(experience.id, rotateState, rotationFactor)}
                     />
                 </ViroARImageMarker>
             )) : null}
@@ -56,7 +131,7 @@ const ViroContent = () => (
     <ViroARSceneNavigator
         autofocus={true}
         initialScene={{
-        scene: InitialScene,
+            scene: InitialScene,
         }}
     />
 );
